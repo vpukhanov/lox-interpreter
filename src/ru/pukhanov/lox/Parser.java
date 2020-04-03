199b08e3e8aae6aa3e982b2e4dd5c1669569defc
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-04-03 20:24:02 +0300

    parser: implements block statement

--- src/ru/pukhanov/lox/Parser.java
+++ src/ru/pukhanov/lox/Parser.java
@@ -45,6 +45,7 @@ class Parser {
 
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
 
@@ -58,6 +59,17 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr expression() {
