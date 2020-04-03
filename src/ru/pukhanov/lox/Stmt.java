09f6b99aa8a94727071556f8ae7280f9f1929cda
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-04-03 19:32:24 +0300

    tooling: generate statement class

---
+++ src/ru/pukhanov/lox/Stmt.java
@@ -0,0 +1,37 @@
+package ru.pukhanov.lox;
+
+abstract class Stmt {
+    abstract <R> R accept(Visitor<R> visitor);
+
+    interface Visitor<R> {
+        R visitExpressionStmt(Expression stmt);
+
+        R visitPrintStmt(Print stmt);
+    }
+
+    static class Expression extends Stmt {
+        final Expr expression;
+
+        Expression(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
+    }
+
+    static class Print extends Stmt {
+        final Expr expression;
+
+        Print(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitPrintStmt(this);
+        }
+    }
+}
