6bb3c31b86f07458e35bbe18cd3ac56ef448ed8d
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2020-04-03 20:14:55 +0300

    interpreter: implements assignment expressions

--- src/ru/pukhanov/lox/Interpreter.java
+++ src/ru/pukhanov/lox/Interpreter.java
@@ -167,4 +167,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         environment.define(stmt.name.lexeme, value);
         return null;
     }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
 }
